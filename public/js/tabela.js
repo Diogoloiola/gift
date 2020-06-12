728ebcc40083ef712ae528b63fceb1ad924607f0
diogo alves <diogoloiola666@gmail.com>
2020-06-12 18:01:44 -0300

    melhorada a forma de como se mostrad os dados

--- public/js/tabela.js
+++ public/js/tabela.js
@@ -5,12 +5,12 @@ let tabela = {
     criaColuna(dados, id) {
         let dataFinal = dados.created_at.slice(0,10);
         return `
-		<tr>
-		<td>${dados.full_name}</td>
-		<td>${dataFinal}</td>
-		<td><select id="op-${id}"><select></td>
-		</tr>
-		`;
+        <tr>
+        <td>${dados.full_name}</td>
+        <td>${dataFinal}</td>
+        <td><select id="op-${id}"><select></td>
+        </tr>
+        `;
     },
     voltarFormulario() {
         $('#tabela').hide();
@@ -19,15 +19,16 @@ let tabela = {
     },
     criaOption(dados, id, url){
         if (dados.length) { 
-            dados.forEach(elemento=>{
-                $(`#op-${id}`).append(`<option value="${url + `/archive/${elemento.tag_name}.zip`}">${elemento.name}</option>`)
-            });
-            console.log(dados[0])
-        } else {
-            console.log(dados)
-            $(`#op-${id}`).append(`<option value="${url + '/archive/master.zip'}">Master</option>`)
-        }
-    }
+           $(`#op-${id}`).append(`<option value="">Selecione</option>`)
+           dados.forEach(elemento=>{
+            $(`#op-${id}`).append(`<option value="${url + `/archive/${elemento.tag_name}.zip`}">${elemento.name}</option>`)
+        });
+
+       } else {
+        $(`#op-${id}`).append(`<option value="">Selecione</option>`)
+           $(`#op-${id}`).append(`<option value="${url + '/archive/master.zip'}">Master</option>`)
+       }
+   }
 }
 
 export { tabela };
