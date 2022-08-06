8737af9ae2ba94405465ac4c0b00ca49a4112c33
Diogo Loiola <diogomouraloiola@gmail.com>
2022-08-05 22:13:17 -0300

    create component input

---
+++ src/components/Input.vue
@@ -0,0 +1,36 @@
+<template>
+  <input
+    class="input"
+    :type="data.inputType"
+    :placeholder="data.placeholder"
+     @input="$emit('update:modelValue', $event.target.value)"
+  />
+</template>
+
+<style scope>
+.input {
+  width: 100%;
+  padding: 10px 15px;
+  border: none;
+  outline: none;
+  border: 1px solid #9eb3c9;
+  border-radius: 5px;
+}
+</style>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const data = defineProps({
+  placeholder: {
+    type: String,
+    required: false,
+  },
+  inputType: {
+    type: String,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["update:modelValue"]);
+</script>
