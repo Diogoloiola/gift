bc67445adf340054d1a9a5605590bcd6a5d896f3
Diogo Loiola <diogomouraloiola@gmail.com>
2022-08-05 22:37:34 -0300

    create form main

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,40 @@
     </nav>
   </header>
   <main class="container">
-    <div>
+    <div class="mt-3">
       <label>Linguagem do repositório</label>
       <custom-input :inputType="'text'" v-model="data.language"></custom-input>
+    </div>
+    <div class="mt-3">
+      <label>Quantidade de forks do repositorio</label>
+      <custom-input :inputType="'text'" v-model="data.forks"></custom-input>
+    </div>
+    <div class="d-flex justify-content-between mt-3">
+      <div class="w-45">
+        <label>Tempo do repositorio</label>
+        <custom-input :inputType="'text'" v-model="data.ageRepository"></custom-input>
+      </div>
+       <div class="w-33">
+        <label>Licença</label>
+        <custom-input :inputType="'text'" v-model="data.licence"></custom-input>
+      </div>
+       <div>
+        <label>Quantidade de estrelas</label>
+        <custom-input :inputType="'text'" v-model="data.starts"></custom-input>
+      </div>
+    </div>
+      <div class="d-flex justify-content-between mt-3 ">
+      <div class="w-50">
+        <label>Tópicos</label>
+        <custom-input :inputType="'text'" v-model="data.topics"></custom-input>
+      </div>
+       <div class="w-50">
+        <label>Tempo de repositório</label>
+        <custom-input :inputType="'text'" v-model="data.ageRepository"></custom-input>
+      </div>
+    </div>
+    <div class="mt-3">
+      <button class="button">Pesquisar</button>
     </div>
   </main>
 </template>
@@ -24,6 +55,19 @@
   width: 98%;
   margin: auto;
 }
+
+.button{
+  padding: 10px 20px;
+  background: transparent;
+  outline: none;
+  border: none;
+  border: 1px solid #343a40;
+  border-radius: 5px;
+}
+
+.button:hover{
+  cursor: pointer;
+}
 </style>
 
 <script setup lang="ts">
@@ -31,7 +75,23 @@ import CustomInput from "./components/Input.vue";
 
 import { reactive } from "@vue/reactivity";
 
-const data = reactive({
+type Params = {
+  language: string;
+  forks: number;
+  sizeRepository: number;
+  licence: string;
+  starts: number;
+  topics: string;
+  ageRepository: number;
+}
+
+const data = reactive<Params>({
   language: "",
+  forks: 0,
+  sizeRepository: 0,
+  licence: '',
+  starts: 0,
+  topics: '',
+  ageRepository: 0,
 });
 </script>
