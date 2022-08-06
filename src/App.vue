52eca2807ea0a3a59d9d7ff61f41c95163c255ba
Diogo Loiola <diogomouraloiola@gmail.com>
2022-08-05 21:48:55 -0300

    create header for site

--- src/App.vue
+++ src/App.vue
@@ -5,27 +5,19 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <header class="header">
+      <nav>
+        <a href="">GitHub Repositories Finder Tool (GiFT)</a>
+      </nav>
+  </header>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  .header{
+    padding: 20px;
+    background-color: #343a40;
+  }
+  .header a{
+    color: #ffff;
+  }
 </style>
