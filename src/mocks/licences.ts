e0b5db06467f23f74f072320a600b8b1f86a012e
Diogo Loiola <diogomouraloiola@gmail.com>
2022-08-05 22:52:33 -0300

    create mock for licences

---
+++ src/mocks/licences.ts
@@ -0,0 +1,133 @@
+export default [{
+  name: '-',
+  value: ''
+},
+{
+  name: 'Apache License 2.0',
+  value: 'apache-2.0'
+},
+{
+  name: 'Artistic license 2.0',
+  value: 'artistic-2.0'
+},
+{
+  name: 'Boost Software License 1.0',
+  value: 'bsl-1.0'
+},
+{
+  name: 'BSD 2-clause Simplified license',
+  value: 'bsd-2-clause'
+},
+{
+  name: 'BSD 3-clause New or Revised license',
+  value: 'bsd-3-clause'
+},
+{
+  name: 'BSD 3-clause Clear license',
+  value: 'bsd-3-clause-clear'
+},
+{
+  name: 'Creative Commons license family',
+  value: 'cc'
+},
+{
+  name: 'Creative Commons Zero v1.0 Universal',
+  value: 'cc0-1.0'
+},
+{
+  name: 'Creative Commons Attribution 4.0',
+  value: 'cc-by-4.0'
+},
+{
+  name: 'Creative Commons Attribution Share Alike 4.0',
+  value: 'cc-by-sa-4.0'
+},
+{
+  name: 'Do What The F*ck You Want To Public License',
+  value: 'wtfpl'
+},
+{
+  name: 'Educational Community License v2.0',
+  value: 'ecl-2.0'
+},
+{
+  name: 'Eclipse Public License 1.0',
+  value: 'epl-1.0'
+},
+{
+  name: 'European Union Public License 1.1',
+  value: 'eupl-1.1'
+},
+{
+  name: 'GNU Affero General Public License v3.0',
+  value: 'agpl-3.0'
+},
+{
+  name: 'GNU General Public License family',
+  value: 'gpl'
+},
+{
+  name: 'GNU General Public License v2.0',
+  value: 'gpl-2.0'
+},
+{
+  name: 'GNU General Public License v3.0',
+  value: 'gpl-3.0'
+},
+{
+  name: 'GNU Lesser General Public License family',
+  value: 'lgpl'
+},
+{
+  name: 'GNU Lesser General Public License v2.1',
+  value: 'lgpl-2.1'
+},
+{
+  name: 'GNU Lesser General Public License v3.0',
+  value: 'lgpl-3.0'
+},
+{
+  name: 'ISC',
+  value: 'isc'
+},
+{
+  name: 'LaTeX Project Public License v1.3c',
+  value: 'lppl-1.3c',
+},
+{
+  name: 'Microsoft Public License',
+  value: 'ms-pl'
+},
+{
+  name: 'MIT',
+  value: 'mit'
+},
+{
+  name: 'Mozilla Public License 2.0',
+  value: 'mpl-2.0'
+},
+{
+  name: 'Open Software License 3.0',
+  value: 'osl-3.0'
+},
+{
+  name: 'PostgreSQL License',
+  value: 'postgresql'
+},
+{
+  name: 'SIL Open Font License 1.1',
+  value: 'ofl-1.1'
+},
+{
+  name: 'University of Illinois/NCSA Open Source License',
+  value: 'ncsa'
+},
+{
+  name: 'The Unlicense',
+  value: 'unlicense'
+},
+{
+  name: 'zLib License',
+  value: 'zlib'
+}
+]
