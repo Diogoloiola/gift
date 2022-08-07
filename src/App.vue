6b44b7ee879e4750eb58dfb436ee3611a5dcdf0f
Diogo Loiola <diogomouraloiola@gmail.com>
2022-08-07 18:08:32 -0300

    add function to prevent event to form

--- src/App.vue
+++ src/App.vue
@@ -5,51 +5,66 @@
     </nav>
   </header>
   <main class="container">
-    <div class="mt-3">
-      <label>Linguagem do repositório</label>
-      <custom-input :inputType="'text'" v-model="data.language"></custom-input>
-    </div>
-    <div class="mt-3">
-      <label>Quantidade de forks do repositorio</label>
-      <custom-input :inputType="'text'" v-model="data.forks"></custom-input>
-    </div>
-    <div class="d-flex justify-content-between mt-3">
-      <div class="w-45">
-        <label>Tempo do repositorio</label>
+    <form @submit.prevent="handleLogin">
+      <div class="mt-3">
+        <label>Linguagem do repositório</label>
         <custom-input
           :inputType="'text'"
-          v-model="data.ageRepository"
+          v-model="data.language"
         ></custom-input>
       </div>
-      <div class="w-33">
-        <label>Licença</label>
-        <select class="input">
-          <option :value=licence.value v-for="(licence, index) in licences" :key="index">
-            {{licence.name}}
-          </option>
-        </select>
+      <div class="mt-3">
+        <label>Quantidade de forks do repositorio</label>
+        <custom-input :inputType="'number'" v-model="data.forks"></custom-input>
       </div>
-      <div>
-        <label>Quantidade de estrelas</label>
-        <custom-input :inputType="'text'" v-model="data.starts"></custom-input>
+      <div class="d-flex justify-content-between mt-3">
+        <div class="w-45">
+          <label>Tempo do repositorio</label>
+          <custom-input
+            :inputType="'number'"
+            v-model="data.ageRepository"
+          ></custom-input>
+        </div>
+        <div class="w-33">
+          <label>Licença</label>
+          <select v-model="data.licence" class="input">
+            <option
+              :value="licence.value"
+              v-for="(licence, index) in licences"
+              :key="index"
+            >
+              {{ licence.name }}
+            </option>
+          </select>
+        </div>
+        <div>
+          <label>Quantidade de estrelas</label>
+          <custom-input
+            :inputType="'text'"
+            v-model="data.starts"
+          ></custom-input>
+        </div>
       </div>
-    </div>
-    <div class="d-flex justify-content-between mt-3">
-      <div class="w-50">
-        <label>Tópicos</label>
-        <custom-input :inputType="'text'" v-model="data.topics"></custom-input>
+      <div class="d-flex justify-content-between mt-3">
+        <div class="w-50">
+          <label>Tópicos</label>
+          <custom-input
+            :inputType="'text'"
+            v-model="data.topics"
+          ></custom-input>
+        </div>
+        <div class="w-50">
+          <label>Tempo de repositório</label>
+          <custom-input
+            :inputType="'number'"
+            v-model="data.ageRepository"
+          ></custom-input>
+        </div>
       </div>
-      <div class="w-50">
-        <label>Tempo de repositório</label>
-        <custom-input
-          :inputType="'text'"
-          v-model="data.ageRepository"
-        ></custom-input>
+      <div class="mt-3">
+        <button class="button">Pesquisar</button>
       </div>
-    </div>
-    <div class="mt-3">
-      <button class="button">Pesquisar</button>
-    </div>
+    </form>
   </main>
 </template>
 
@@ -94,7 +109,6 @@ import CustomInput from "./components/Input.vue";
 import { reactive } from "@vue/reactivity";
 import licences from "./mocks/licences";
 
-console.log(licences);
 type Params = {
   language: string;
   forks: number;
@@ -114,4 +128,8 @@ const data = reactive<Params>({
   topics: "",
   ageRepository: 0,
 });
+
+function handleLogin(): void {
+  console.log(data.forks);
+}
 </script>
