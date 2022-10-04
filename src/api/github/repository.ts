2b4f2d9f32a633492b71fa5018a36c8e6ec4fba5
Diogo Loiola <diogomouraloiola@gmail.com>
2022-10-04 20:41:44 -0300

    added new method that search repository for name

--- src/api/github/repository.ts
+++ src/api/github/repository.ts
@@ -2,7 +2,11 @@ import axiosClient from './axiosInstance';
 
 class Repository {
   async getAll(params: Record<string, unknown>): Promise<Result> {
-    return await (await axiosClient.get('/repositories', { params: params })).data;
+    return (await axiosClient.get('/search/repositories', { params: params })).data;
+  }
+
+  async findByName(organization: string, name: string): Promise<Project> {
+    return (await axiosClient.get(`/repos/${organization}/${name}`)).data; 
   }
 }
 
