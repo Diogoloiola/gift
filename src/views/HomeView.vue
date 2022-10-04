7068760d83f2121b5e58bcd3d2121d22662e59b6
Diogo Loiola <diogomouraloiola@gmail.com>
2022-10-04 18:37:22 -0300

    added form do home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,56 +5,7 @@
     </nav>
   </header>
   <main class="container">
-    <form @submit.prevent="handleLogin">
-      <div class="mt-3">
-        <label>Linguagem do repositório</label>
-        <custom-input
-          :inputType="'text'"
-          v-model="data.language"
-        ></custom-input>
-      </div>
-      <div class="mt-3">
-        <label>Quantidade de forks do repositorio</label>
-        <custom-input :inputType="'number'" v-model="data.forks"></custom-input>
-      </div>
-      <div class="d-flex justify-content-between mt-3">
-        <div class="w-45">
-          <label>Tamanho do repositorio</label>
-          <custom-input
-            :inputType="'number'"
-            v-model="data.size"
-          ></custom-input>
-        </div>
-        <div class="w-33">
-          <label>Licença</label>
-          <select v-model="data.license" class="input">
-            <option
-              :value="licence.value"
-              v-for="(licence, index) in licences"
-              :key="index"
-            >
-              {{ licence.name }}
-            </option>
-          </select>
-        </div>
-        <div>
-          <label>Quantidade de estrelas</label>
-          <custom-input :inputType="'text'" v-model="data.stars"></custom-input>
-        </div>
-      </div>
-      <div class="d-flex justify-content-between mt-3">
-        <div class="w-50">
-          <label>Tópicos</label>
-          <custom-input
-            :inputType="'text'"
-            v-model="data.topics"
-          ></custom-input>
-        </div>
-      </div>
-      <div class="mt-3">
-        <button class="button">Pesquisar</button>
-      </div>
-    </form>
+    <Form v-if="formIsVisible" @searchProjects='searchProjects' />
   </main>
 </template>
 
@@ -63,64 +14,27 @@
   padding: 20px;
   background-color: #343a40;
 }
+
 .header a {
   color: #ffff;
 }
+
 .container {
   width: 98%;
   margin: auto;
 }
 
-.button {
-  padding: 10px 20px;
-  background: transparent;
-  outline: none;
-  border: none;
-  border: 1px solid #343a40;
-  border-radius: 5px;
-}
-
-.button:hover {
-  cursor: pointer;
-}
-.input {
-  width: 100%;
-  padding: 10px 15px;
-  border: none;
-  outline: none;
-  border: 1px solid #9eb3c9;
-  border-radius: 5px;
-  background: #fff;
-}
 </style>
 
 <script setup lang="ts">
-import CustomInput from "../components/Input.vue";
-import { reactive } from "@vue/reactivity";
-import licences from "../mocks/licences";
-import createQueryForRequest from "../api/github/utils";
+import Form from "./Form.vue";
 import { Client } from "../api/github/client";
+import { ref } from "vue";
 
-type Params = {
-  language: string;
-  forks: number;
-  size: number;
-  license: string;
-  stars: number;
-  topics: string;
-};
+const formIsVisible = ref(true);
 
-const data = reactive<Params>({
-  language: "",
-  forks: 0,
-  size: 0,
-  license: "",
-  stars: 0,
-  topics: "",
-});
-
-async function handleLogin(): Promise<void> {
+async function searchProjects(query: string): Promise<void> {
   const client = new Client();
-  const result = await client.repositories().getAll({ q: createQueryForRequest(data), per_page: 100 });
+  const result = await client.repositories().getAll({ q: query, per_page: 100 });
 }
 </script>
