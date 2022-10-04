04388d8b5d5047c2f8c1ee63f5a37e2ad21cad69
Diogo Loiola <diogomouraloiola@gmail.com>
2022-10-04 19:49:17 -0300

    added table to view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,12 @@
   </header>
   <main class="container">
     <Form v-if="formIsVisible" @searchProjects='searchProjects' />
+    <AppTable v-else :headers="['Nome projeto', 'Descrição']">
+      <tr v-for="(project, id) in projects?.items" :id="`${id}`">
+        <td>{{project.name}}</td>
+        <td>{{project.description}}</td>
+      </tr>
+    </AppTable>
   </main>
 </template>
 
@@ -22,19 +28,37 @@
 .container {
   width: 98%;
   margin: auto;
+  margin-top: 20px;
 }
-
 </style>
 
 <script setup lang="ts">
-import Form from "./Form.vue";
-import { Client } from "../api/github/client";
 import { ref } from "vue";
+import Form from "./Form.vue";
+
+import { Client } from "../api/github/client";
+import AppTable from "../components/AppTable.vue";
 
 const formIsVisible = ref(true);
 
+type RequestForGithubApi = {
+  q: string,
+  per_page: number,
+  page: number,
+}
+
+const data = ref<RequestForGithubApi>({
+  q: '',
+  per_page: 15,
+  page: 1
+});
+
+const projects = ref<Result>();
+
 async function searchProjects(query: string): Promise<void> {
   const client = new Client();
-  const result = await client.repositories().getAll({ q: query, per_page: 100 });
+  data.value.q = query;
+  projects.value = await client.repositories().getAll(data.value);
+  formIsVisible.value = false;
 }
 </script>
