8f45823936f2c5c6b5fc8dcf2f649459339b789a
Diogo Loiola <diogomouraloiola@gmail.com>
2022-10-18 19:35:29 -0300

    create componente button

---
+++ src/components/AppRoundedButton.vue
@@ -0,0 +1,34 @@
+<template>
+    <button class="btn btn-gray" @click="handleClick">{{text}}</button>
+</template>
+
+<style scoped>
+.btn {
+    display: block;
+    width: 100%;
+    padding: 15px;
+    outline: none;
+    border: none;
+    border-radius: 50px;
+}
+
+.btn:hover {
+    cursor: pointer;
+}
+
+.btn-gray {
+    background-color: #808080;
+    color: #fff;
+}
+</style>
+
+<script lang="ts" setup>
+  type Props = {
+    text: string,
+    back: () => void;
+  }
+
+  const props = defineProps<Props>();
+
+  const handleClick = () => props?.back();
+</script>
