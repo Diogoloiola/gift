295eccc88693277264f84bcb283d22f0d7004e8d
Diogo Loiola <diogomouraloiola@gmail.com>
2022-12-01 15:20:44 -0300

    fix route for details repo info

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,7 @@
   <main class="container">
     <Form v-if="formIsVisible" @searchProjects='searchProjects' />
     <AppTable v-else :headers="['Nome projeto', 'Descrição']" :total_count="projects?.total_count" :back="backToForm" >
-      <tr v-for="(project, id) in projects?.items" :id="`${id}`" @click="$router.push(`resume/${project.full_name}`)">
+      <tr v-for="(project, id) in projects?.items" :id="`${id}`" @click="$router.push(`details/${project.full_name}`)">
         <td>{{project.name}}</td>
         <td>{{project.description}}</td>
       </tr>
